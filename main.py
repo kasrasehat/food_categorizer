924116d23a0c9eb05938c69ed912dda75f1a6fc5
Kasra Sehat <kasra.sehat@gmail.com>
2025-12-22 18:59:35 +0330

    create first endpoint for finding food group for single word

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from app.api.v1.router import api_router, lifespan
+from utils.settings import get_settings
+import uvicorn
+
+
+def create_app() -> FastAPI:
+    settings = get_settings()
+
+    app = FastAPI(
+        title=settings.APP_NAME,
+        version=settings.APP_VERSION,
+        lifespan=lifespan,
+    )
+    app.include_router(api_router)
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
